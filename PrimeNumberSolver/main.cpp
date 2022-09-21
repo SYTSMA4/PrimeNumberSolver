833558fc1b00199b1f766ca8fa65edf5e14873d9
SYTSMA4 <sytsma4@tritons.iowacentral.edu>
2022-09-21 10:32:57 -0500

    Initial Commit

---
+++ PrimeNumberSolver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PrimeNumberSolver
+//
+//  Created by Sam Sytsma on 9/21/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
