8918cbaadd412e89196eb7357fc615475058734a
SYTSMA4 <sytsma4@tritons.iowacentral.edu>
2022-09-21 10:54:11 -0500

    Update main.cpp

--- PrimeNumberSolver/main.cpp
+++ PrimeNumberSolver/main.cpp
@@ -1,14 +1,27 @@
 //
-//  main.cpp
-//  PrimeNumberSolver
-//
 //  Created by Sam Sytsma on 9/21/22.
 //
 
 #include <iostream>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    int x = 1000;
+    
+    cout << "Please input a max value of 1000: ";
+    cin >> x;
+    
+    for ( int m=2; m<x; m++)         //goes through numbers 2-1000
+    for ( int n=2; n*n<=m; n++)      //tests all numbers for each previous numbers
+    {
+        if ( m % n == 0)
+            break;
+        else if ( n+1 > sqrt (m)) {
+            cout << m << endl;
+        }
+    }
+    
     return 0;
 }
